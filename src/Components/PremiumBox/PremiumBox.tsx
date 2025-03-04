f2685c94c1154e7339a1cb2374790b647df9a30e
PhucONT <phucontse172629@fpt.edu.vn>
2025-03-04 20:02:11 +0700

    update

---
+++ src/Components/PremiumBox/PremiumBox.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./PremiumBox.module.css";
+import { ReactComponent as CheckIcon} from "../../assets/Icons/check.svg";
+import { ReactComponent as DisabledCheckIcon} from "../../assets/Icons/cross.svg";
+
+
+interface PremiumBoxProps {
+  title: string;
+  price: string;
+  originalPrice?: string;
+  isPopular?: boolean;
+  features: string[];
+}
+
+const PremiumBox: React.FC<PremiumBoxProps> = ({ title, price, originalPrice, isPopular, features }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.premiumContaier}>
+        <div className={styles.premiumBox}>
+          <div className={styles.header}>
+            {isPopular && <span className={`${styles.popularBadge} s9`}>Phổ biến nhất</span>}
+          </div>
+          <h3 className={styles.price}>{price}</h3>
+          {originalPrice && (
+            <p className={`${isPopular ? styles.originalPricePopular : styles.originalPrice} s7`}>
+              {title === "Gói miễn phí" ? "Miễn phí" : title === "Gói tiết kiệm" ? "Tiết kiệm nhất" : "Giá gốc "} 
+              <span className={styles.strikethrough}>{originalPrice}</span>
+            </p>
+          )}
+          <button className={`${isPopular ? styles.selectButtonPopular : styles.selectButton} s6`}>Chọn gói</button>            
+          <div className={styles.divider}></div>
+          <ul className={styles.featuresList}>
+            {features.map((feature, index) => {
+              const isDisabled = feature.startsWith("-");
+              return (
+                <li key={index} className={isDisabled ? styles.disabledFeature : styles.enabledFeature}>
+                  {isDisabled ? <DisabledCheckIcon className={styles.disabledIcon} /> : <CheckIcon className={styles.CheckIcon} />}
+                  {feature.replace(/^- /, "")}
+                </li>
+              );
+            })}
+          </ul>   
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+const PremiumPlans = () => {
+  return (
+    <div className={styles.premiumContainer}>
+      <PremiumBox 
+        title="Gói miễn phí"
+        price="0đ/tháng"
+        features={[
+          "Mở khóa toàn bộ bài tập",
+          "10 câu hỏi với AI mỗi ngày",
+          "- Cá nhân hóa nội dung học tập",
+          "- Liên tục cập nhật đề thi mới",
+          "- Thi thử như thi thật"
+        ]}
+      />
+
+      <PremiumBox 
+        title="Gói tiết kiệm"
+        price="45.000đ/tháng"
+        features={[
+          "Mở khóa toàn bộ bài tập",
+          "AI giải đáp toàn diện câu hỏi",
+          "Cá nhân hóa nội dung học tập",
+          "Liên tục cập nhật đề thi mới",
+          "Thi thử như thi thật"
+        ]}
+      />
+
+      <PremiumBox 
+        title="Phổ biến nhất"
+        price="144.000đ/kỳ"
+        originalPrice="180.000đ"
+        isPopular
+        features={[
+          "Mở khóa toàn bộ bài tập",
+          "AI giải đáp toàn diện câu hỏi",
+          "Cá nhân hóa nội dung học tập",
+          "Liên tục cập nhật đề thi mới",
+          "Thi thử như thi thật"
+        ]}
+      />
+    </div>
+  );
+};
+
+export default PremiumPlans;
--- src/pages/Upgrade/Upgrade.tsx
+++ src/pages/Upgrade/Upgrade.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from '../../Components/Navbar/Navbar'
+import PremiumBox from '../../Components/PremiumBox/PremiumBox'
 import Footer from '../../Components/Footer/Footer'
+
+
 
 
 const Upgrade: React.FC = () => {
   return (
     <div className="container">
       <Navbar />
+      <PremiumBox />
       <Footer />
     </div>
   );
