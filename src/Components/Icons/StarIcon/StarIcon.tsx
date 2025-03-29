42c179266a77709bec3826cb6e9c3aeebf550e1e
PhucONT <phucontse172629@fpt.edu.vn>
2025-03-29 20:10:33 +0700

    FixingStarIcon

--- src/Components/Icons/StarIcon/StarIcon.tsx
+++ src/Components/Icons/StarIcon/StarIcon.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import { ReactComponent as StarSvg } from "../../../assets/Icons/star.svg"; 
-import styles from "./StarIcon.module.css"; 
+import styles from "./StarIcon.module.css";
 
 const StarIcon: React.FC = () => {
     const [marked, setMarked] = useState(false);
@@ -10,10 +9,20 @@ const StarIcon: React.FC = () => {
     };
 
     return (
-        <StarSvg 
-            className={`${styles.star} ${marked ? styles.filled : ""}`}
+        <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 24 24" 
+            fill={marked ? "yellow" : "none"} 
+            stroke={marked ? "yellow" : "gray"} 
+            strokeWidth="2"
+            width="24px" 
+            height="24px" 
+            className={styles.star} 
             onClick={handleToggle}
-        />
+            style={{ cursor: "pointer" }}
+        >
+            <path d="m12 17.328-5.403 3.286a.75.75 0 0 1-1.12-.813l1.456-6.155-4.796-4.123a.75.75 0 0 1 .428-1.316l6.303-.517 2.44-5.835a.75.75 0 0 1 1.384 0l2.44 5.835 6.303.517a.75.75 0 0 1 .427 1.316l-4.795 4.123 1.456 6.155a.75.75 0 0 1-1.12.813L12 17.328z" />
+        </svg>
     );
 };
 
