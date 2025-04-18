65254d37cf922050ca052a46c67289ac0c959cc4
PhucONT <phucontse172629@fpt.edu.vn>
2025-04-18 09:52:29 +0700

    update alias

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,13 @@ export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
     alias: {
+      "@src": path.resolve(__dirname, "src"),
       "@assets": path.resolve(__dirname, "src/assets"),
       "@Components": path.resolve(__dirname, "src/Components"),
       "@data": path.resolve(__dirname, "src/data"),
       "@pages": path.resolve(__dirname, "src/pages"),
       "@audio": path.resolve(__dirname, "public/audio"),
+      "@redux": path.resolve(__dirname, "src/redux"),
     }
   }
 })
